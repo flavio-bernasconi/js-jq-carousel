65ed7903301db3fa1ded6c9da4cc7be82602e330
flavio-bernasconi <info@flaviobernasconi.it>
2019-07-19 15:13:50 +0200

    bullets nav

--- main.js
+++ main.js
@@ -6,14 +6,18 @@ $(".next").click(successivo);
 
 function successivo(){
   var immagine = $(".container-slider img.active");
+  var bull = $(".bullets span.active");
 
   immagine.removeClass("active");
+  bull.removeClass("active");
 
-  if (immagine.hasClass("last")) {
-    $("img.first").addClass("active")
+  if (immagine.hasClass("last") && bull.hasClass("last")) {
+    $("img.first").addClass("active");
+    $("span.first").addClass("active")
   }
   else {
     immagine.next().addClass("active");
+    bull.next().addClass("active");
   }
 }
 
@@ -22,14 +26,19 @@ $(".prev").click(precedente);
 
 function precedente(){
   var immagine = $(".container-slider img.active");
+  var bull = $(".bullets span.active");
+
 
   immagine.removeClass("active");
+  bull.removeClass("active");
 
   if (immagine.hasClass("first")) {
     $("img.last").addClass("active")
+    $("span.last").addClass("active")
   }
   else {
     immagine.prev().addClass("active");
+    bull.prev().addClass("active");
   }
 }
 
